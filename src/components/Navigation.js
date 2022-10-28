1c102f4741209a59a02dc2bbaa084c54c127dd1b
seapearl98 <gown1977@gmail.com>
2022-10-28 18:13:32 +0900

    four

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,20 +7,24 @@ export default function Navigation({userObj}) {
 
 
   return (
-    <nav>
-        <ul style={{display: "flex", justifyContent: "center", marginTop:50}}>
-            <li>
-                <Link to={'/'} style={{marginRight:10}}><FontAwesomeIcon icon="fa-brands fa-twitter"  color='#04aaff' size='2x' /></Link></li>
-            <li>
-              <Link to={'/profile'}
-                  style={{display: "flex", flexDirection: "column", alignItems: "center", marginLeft:10, fontSize:12}}>
-                  <FontAwesomeIcon icon="fa-solid fa-user"  color='#04aaff' size='2x' />
-                  <span style={{marginTop:10}}>
-                  {userObj.displayName ? `${userObj.displayName}의 profile` : "Profile"}
-                </span>
-              </Link>
-            </li>
-        </ul>
+<nav>
+      <ul style={{display: 'flex',justifyContent:'center', marginTop: 50}}>
+        <li>
+          <Link to={"/"} style={{marginRight: 10}}>
+            <FontAwesomeIcon icon="fa-brands fa-twitter" color={'#04aaff'} size='2x'/>
+          </Link> 
+        </li>
+        <li>
+          <Link to={"/profile"} style={{display: 'flex',flexDirection: 'column',alignItems:"center", marginLeft:10, fontSize:12}}>
+          {userObj.photoURL && (
+            <img src={userObj.photoURL} width='50' height='50' style={{borderRadius:50}}/>
+          )}
+          <span style={{marginTop:10}}>
+            {userObj.displayName ? `${userObj.displayName}의 Profile` : "Profile"}
+          </span>
+          </Link>        
+        </li>
+      </ul>
     </nav>
   )
 }
--- src/routes/Profiles.js
+++ src/routes/Profiles.js
@@ -2,18 +2,19 @@ import React, { useEffect,useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { authService,db,storage } from '../fbase';
 import Tweet from '../components/Tweet';
-import { collection, addDoc, query, onSnapshot,getDocs,where,orderBy } from "firebase/firestore";
+import { collection, query, onSnapshot,where,orderBy } from "firebase/firestore";
 import { updateProfile } from "firebase/auth";
 import { v4 as uuidv4 } from 'uuid';
 import { ref, uploadString, getDownloadURL } from "firebase/storage";
-import "styles/profiles.scss"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import '../styles/profiles.scss'
 
 
 function Profiles({userObj}) {
   const [tweets, setTweets] = useState([]); 
   const navigate = useNavigate();
   const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
-  const [newPhoto,setNewPhoto] = useState(userObj.photoURL);
+  const [newPhotoURL,setNewPhotoURL] = useState(userObj.photoURL);
  
   const onLogOutClick = () => {
     authService.signOut();
@@ -40,7 +41,7 @@ function Profiles({userObj}) {
     reader.onloadend = (finishedEvent) => {
       console.log(finishedEvent);
       const {currentTarget: {result}} = finishedEvent;
-      setNewPhoto(result);
+      setNewPhotoURL(result);
     }
     reader.readAsDataURL(theFile)
   }
@@ -53,9 +54,9 @@ function Profiles({userObj}) {
   const onSubmit = async(e) => {
     e.preventDefault();
     let photoURL = "";
-    if(userObj.photoURL != newPhoto){
+    if(userObj.photoURL != newPhotoURL){
       const storageRef = ref(storage, `${userObj.uid}/${uuidv4()}`);
-      const response = await uploadString(storageRef, newPhoto, 'data_url');
+      const response = await uploadString(storageRef, newPhotoURL, 'data_url');
       console.log(response)
       photoURL = await getDownloadURL(ref(storage, response.ref))
       await updateProfile(userObj, {photoURL});
@@ -65,33 +66,54 @@ function Profiles({userObj}) {
       await updateProfile(userObj, {displayName: newDisplayName});
     }
   }
-  const onClearAttachment = () => setNewPhoto("")
 
-  useEffect(()=>{ //실시간 데이터베이스 문서들 가져오기
-    // getTweets();
-    const q = query(collection(db, "tweets" ),
+  
+  const onClearAttachment = () => setNewPhotoURL("")
+
+  useEffect(() => {
+    //getMyTweets();
+    const q = query(collection(db, "tweets"),
     where("createId", "==", userObj.uid),
     orderBy("createAt", "desc"));
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
       const newArray = [];
       querySnapshot.forEach((doc) => {
         newArray.push({...doc.data(), id:doc.id});
-      })
-      console.log(newArray)
-      setTweets(newArray)
-    })
+      });
+      setTweets(newArray);
+    });
   },[]);
   
   return (
     <div className='container'>
-      <form onSubmit={onSubmit} className="profileForm">
-        <input type="text" placeholder='Display name' 
-        onChange={onChange} value={newDisplayName}
-        autoFocus className='formInput'/>
-        <input type="submit" value="Update Profile" 
-        className='formBtn' style={{marginTop:10,}}/>
+      <form onSubmit={onSubmit} className='profileForm'>
+        <input type="text" placeholder='Display name' onChange={onChange} value={newDisplayName} autoFocus className='formInput'/>
+        <input type="submit" value="Update Profile" className='formBtn' style={{marginTop:10}}/>
+        <label htmlFor='attach-file' className='factoryInput__lable'>
+        <span>Add Pofile photo</span>
+      <FontAwesomeIcon icon="fa-solid fa-plus" />
+      </label>
+      <input type="file" accept='image/*' onChange={onFileChange} id='attach-file' style={{opacity:0}}/>
+        {newPhotoURL && (
+        <div className='profileForm__newPhotoURL'>
+          <img src={newPhotoURL} style={{backgroundImage: newPhotoURL}} />
+          <div className='profileForm__clear' onClick={onClearAttachment}>
+            <span>Remove</span>
+            <FontAwesomeIcon icon="fa-solid fa-xmark" />
+          </div>
+        </div>
+        )}
       </form>
-      <span onClick={onLogOutClick} className="cancleBtn">Log Out</span>
+      <span className='formBtn cancleBtn logOut' onClick={onLogOutClick}>Log Out</span>
+      <div>
+      {tweets.map(tweet => (
+        <Tweet 
+        key={tweet.id}
+        tweetObj={tweet}
+        isOwner={tweet.createId === userObj.uid}
+        />
+      ))}
+      </div>
     </div>
   )
 }
